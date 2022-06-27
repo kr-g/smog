af13494e936cca0bbeb5775450425fa24f7300b1
kr-g <k.r.goger@gmail.com>
2022-06-27 07:55:40 +0200

    cleanup

--- smog/movefin.py
+++ smog/movefin.py
@@ -44,28 +44,3 @@ def organize_move_processed_file(src, base, rel_store):
         )
 
     return fsrc.name, fstore.name
-
-
-if __name__ == "__main__":
-
-    for f, b, s in [
-        ("~/Bilder/20220521.jpeg", "~/Bilder", "../../proc-media"),
-        ("~/Bilder/20220521.jpeg", "~/Bilder", "../Bilder/"),
-        ("~/Bilder/20220521.jpeg", "~/Bilder", make_timed_rel_store_path()),
-        ("~/Bilder/20220521.jpeg", "~/Bilder", "//proc-media"),
-        (
-            "~/Bilder/20220508-0609 pfalz/IMG_20220508_172822.jpg",
-            "~/Bilder",
-            make_timed_rel_store_path(),
-        ),
-        (
-            "~/Bilder/20220508-0609 pfalz/IMG_20220508_172822.jpg",
-            "~/Bilder",
-            "/proc-media",
-        ),
-    ]:
-        try:
-            rc = organize_move_processed_file(f, b, s)
-            print(rc)
-        except Exception as ex:
-            print(ex)
