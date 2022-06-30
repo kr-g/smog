bc221393582de2f05b73596c4fb679d96fd32e1a
kr-g <k.r.goger@gmail.com>
2022-06-30 05:50:39 +0200

    refactored context handling

--- smog/context.py
+++ smog/context.py
@@ -46,64 +46,21 @@ class Context(object):
         return fnam[len(self.srcdir) + 1 :]
 
 
-class CtxRunner(object):
-    def __init__(self, ctx, input_iter):
+class CtxProcessor(object):
+    def process(self, ctx, inp, err):
+        raise NotImplementedError()
+
+
+class CtxPipe(object):
+    def __init__(self, ctx):
         self.ctx = ctx
-        self.input_iter = input_iter
+        self.chain = []
 
-        self.proc = None
-        self.output = None
+    def add(self, ctx_runner):
+        self.chain.append(ctx_runner)
 
-        self.errproc = []
-        self.errout = []
-
-    def config_run(self, proc, output, errhndl=None, errouthndl=None):
-        self.proc = proc
-        self.output = output
-        self.errhndl = errhndl if errhndl else self.errproc_handler
-        self.errouthndl = errouthndl if errouthndl else self.errout_handler
-
-    def run(self):
-        for inp in self.input_iter():
-
-            rc, err, ex = self.do_proc(inp)
-            if ex:
-                print(ex)
-            if err:
-                err = self.errhndl(err)
-            if err or ex:
-                continue
-
-            rc, err, ex = self.do_output(inp)
-            if ex:
-                print(ex)
-            if err:
-                err = self.errhndl(err)
-            if err or ex:
-                continue
-
-        return self.errproc, self.errout
-
-    def do_proc(self, inp):
-        try:
-            rc, err = self.proc(inp, self)
-            return rc, err, None
-        except Exception as ex:
-            return None, inp, ex
-
-    def errproc_handler(self, err):
-        self.errproc.append(err)
-        print("err", err)
-        return True
-
-    def do_output(self, inp):
-        try:
-            rc, err = self.output(inp, self)
-            return rc, err, None
-        except Exception as ex:
-            return None, inp, ex
-
-    def errout_handler(self, err):
-        self.errout.append(err)
-        print("err", err)
-        return True
+    def process(self, inp=None):
+        for cproc in self.chain:
+            inp, err = cproc.process(self.ctx, inp, err)
+            if inp is None and err is None:
+                break
