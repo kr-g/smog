590eaf9906481e143db23ffea41af81a3f13490d
kr-g <k.r.goger+github@gmail.com>
2022-07-30 20:10:25 +0200

    setup rework

--- run_prep.py
+++ run_prep.py
@@ -1,6 +1,8 @@
 from run_freeze import bump_requirements
 from run_autodoc import create_autodoc
+from run_upgrade import upgrade_requirements_packages
 
 if __name__ == "__main__":
+    upgrade_requirements_packages()
     create_autodoc()
     bump_requirements()
