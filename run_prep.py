ee3c601c532b00248cbc47e9db48415c790dd1f9
kr-g <k.r.goger+github@gmail.com>
2022-07-30 23:38:35 +0200

    skip build

--- run_prep.py
+++ run_prep.py
@@ -3,6 +3,6 @@ from run_autodoc import create_autodoc
 from run_upgrade import upgrade_requirements_packages
 
 if __name__ == "__main__":
-    upgrade_requirements_packages()
+    # upgrade_requirements_packages()
     create_autodoc()
     bump_requirements()
